d72a2c47f121db88040351dbddd0bcb29c8df743
archer1729 <rinkujagtap003@gmail.com>
2024-10-17 13:24:15 +0530

    variables file updated

---
+++ 01_basics/variables.js
@@ -0,0 +1,38 @@
+// there are three types you can use variables in javascript..
+// 1. let
+// 2. const
+// 3. var 
+
+//if u declare variable using let then you can re-assign it.
+// let is block-scoped.
+//example..
+ let accountEmail="rinkujagtap003@gmail.com";
+
+ // if u declare variable using const then you cant re-assign or re-declare it.
+ // const is block-scoped.
+ //example..
+ const accountId=2024301;
+
+
+ //now-a-days we don't use var to declare variables due to block-scope and functional-scope issues.
+ //example..
+ var accPassword="root";
+
+// lets try re-assigning the values for variables and see the out put.
+//Even, if u re-assign the value ,it wont change.
+ //const accountId=2024302;
+ // above code of line shows error.
+
+ //let accountEmail="rinkujagtap56@gmail.com";
+// above code of line shows syntax error.
+
+// if u don't assign value to var it will show as "undefined" .
+
+
+ var accPassword=""
+ // when we declare using var then assigned recent value is printed.
+
+
+ console.log(accPassword);
+ console.log(accountId);
+ console.log(accountEmail);
