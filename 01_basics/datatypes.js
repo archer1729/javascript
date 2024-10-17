ba93d8ab9c789739e5e5112797986482eb46f182
archer1729 <rinkujagtap003@gmail.com>
2024-10-17 14:02:37 +0530

    datatypes file updated

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+//lets disscuss datatypes..
+//mostly used data types are..
+//1. int- indicates numbers like 1,2,3,4...
+let age= 5;
+//2. float- indicates decimal numbers like 3.4, 5.6,    
+let lenght=12.5;
+//3.string- indicates words..
+let Name="Rinku";  //note :use quotes to assign a string value.
+//4.boolean- indicates true or false..
+let isAdmin=true;
+//5.array- indicates a collection of values..
+let colors=["red","green","blue"];
+//6.object- indicates a collection of key-value pairs..
+let person={name:"Rinku",age:25};
+//7.null-it is a standalone value ..
+//8.undefined- it indicates that value is not assigned yet.
+
+//lets print them...
+console.table([age,length=12.5,Name,isAdmin,colors,person])
